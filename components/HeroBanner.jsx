1cd4d2bc94375bb118b2900b8bbbdaa2c97e134f
abstract⁷ <24471792+lexusvp@users.noreply.github.com>
2024-12-13 18:25:50 +0800

    Update HeroBanner.jsx

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,40 +1,41 @@
-import React from 'react'
-import Image from 'next/image'
-import {CgShoppingCart} from 'react-icons/cg'
-import headerImg from '../src/assets/header.png'
-import featured1 from '../src/assets/Featured1.png';
-import featured2 from '../src/assets/Featured2.png';
-import featured3 from '../src/assets/Featured3.png';
-import featured4 from '../src/assets/Featured4.png';
+import React from 'react' 
+import {CgShoppingCart} from 'react-icons/cg' 
 import Link from 'next/link';
 
 const HeroBanner = () => {
+ 
+ 
   return (
-    <header className='header'>
+    <header className="header">
+  
+            {/* Background Layer */}
+            <div className="background-layer"></div>
+
         <div className='header-left-side'>
-            <div className='header-content'>
-                <span>Sale 70%</span>
-                <h1>An Industrial Take on Streetwear</h1>
-                <p>Anyone can beat you but no one can beat your outfit as long as you wear Dine outfits.</p>
+            <div className='header-content'> 
+                <h1>CitrixLab Philippines:</h1>
+                <p>&nbsp;Your Trusted IT Solution Provider</p> 
+                <p>&nbsp;</p> 
+                <small>
+                  At CitrixLab, we provide expert Citrix and NetScaler solutions through training, 
+                  on-demand labs, and tailored IT support to optimize your IT infrastructure and drive 
+                  business performance.
+                </small> 
+                <p>&nbsp;</p> 
                 <Link href='/products'>
-                     <button className='btn' type='button'><CgShoppingCart size={26} />  Start Shopping</button>
-                </Link>
+                     <button className='btn' type='button'><CgShoppingCart size={26} />  Learn More</button>
+                </Link> 
+                <p>&nbsp;</p> 
+                <p>&nbsp;</p> 
             </div>
 
-            <div className='header-featured'>
-                <Image src={featured1} width={100} height={35} alt='img' />
-                <Image src={featured2} width={100} height={35} alt='img' />
-                <Image src={featured3} width={100} height={35} alt='img' />
-                <Image src={featured4} width={100} height={35} alt='img' />
-            </div>
         </div>
 
-        <div className='header-right-side'>
-            <div className='header-circle'>
-                <Image className='header-img' src={headerImg} width={650} height={650} alt='header image' />
-            </div>
+        <div className='header-right-side'> 
         </div>
+
     </header>
+    
   )
 }
 
