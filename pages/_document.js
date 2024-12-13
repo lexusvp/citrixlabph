2b5dff4eb3bb6e4de58edaec6d2231b551f86d90
abstract⁷ <24471792+lexusvp@users.noreply.github.com>
2024-12-13 18:35:12 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head>
-        <title>JoHaOn Online Boutique</title>
+    <Html lang="en"> 
       <Head />
       <body>
         <Main />
